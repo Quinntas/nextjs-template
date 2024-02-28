d13901158bb94fae34e93dfffa2a2e8ab26959d2
Caio "Quintas_" Santiago <caioquintassantiago@gmail.com>
2024-02-28 14:56:42 -0300

    chore: fix provider folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type {Metadata} from "next";
 import {Inter as FontSans} from "next/font/google"
 import "./globals.css";
 import {cn} from "@/lib/utils";
-import {ThemeProvider} from "@/components/theme-provider";
+import {ThemeProvider} from "@/lib/providers/theme-provider";
 import {Toaster} from "@/components/ui/sonner"
-import {AuthSessionProvider} from "@/lib/auth";
+import {AuthSessionProvider} from "@/lib/providers/auth-provider";
 
 const fontSans = FontSans({
     subsets: ["latin"],
