efb265530ba0796451d753d6bad9ab560361c638
quintas <caioquintassantiago@gmail.com>
2024-11-07 16:22:51 -0300

    fix db and add stripe

--- src/lib/drizzle/database.ts
+++ src/lib/drizzle/database.ts
@@ -1,6 +1,3 @@
 import {drizzle} from 'drizzle-orm/postgres-js'
-import postgres from "postgres";
 
-export const db = drizzle(
-    postgres(process.env.DATABASE_URL!, {prepare: false}),
-)
+export const db = drizzle(process.env.DATABASE_URL!);
